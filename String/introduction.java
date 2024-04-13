d44b3074e7b770c55fe35d298e37a70bbcd99ac9
HiimanshuVaatsal <131615256+HiimanshuVaatsal@users.noreply.github.com>
2024-04-13 06:51:24 +0000

    done

---
+++ String/introduction.java
@@ -0,0 +1,15 @@
+package String;
+
+public class introduction {
+   public static void main (String[]ags){
+    String str="abcd";
+    String str2="abcd";
+    String str3 = new String("abcd");
+    
+    if(str==str3){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+}
+}
